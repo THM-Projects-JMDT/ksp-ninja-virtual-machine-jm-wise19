483f7bcf26b6dcd24bc4788a085619015ec52abb
Jannik Lapp <jannik.lapp@mni.thm.de>
2019-10-14 14:57:20 +0200

    chnaged comment to version 99

--- src/njvm.c
+++ src/njvm.c
@@ -2,20 +2,20 @@
 #include <string.h>
 
 const char *version = "0";
-/* Save Build Date & Time */
+// Save Build Date & Time
 const char *buildDT = __DATE__ ", " __TIME__;
 
 int main(int argc, char *argv[])
 {
     for (int i = 1; i < argc; i++)
     {
-        /* argument version: */
+        // argument version:
         if (strcmp(argv[i], "--version") == 0)
         {
             printf("Ninja Virtual Machine Version %s (compiled %s)\n", version, buildDT);
             return 0;
         }
-        /* argument help: */
+        // argument help:
         else if (strcmp(argv[i], "--help") == 0)
         {
             printf("usage: %s [options] ...\n"
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
                    argv[0]);
             return 0;
         }
-        /* invalid argument */
+        // invalid argument
         else
         {
             printf("unknown command line argument '%s', try '%s --help'\n", argv[i], argv[0]);
@@ -36,6 +36,6 @@ int main(int argc, char *argv[])
     // Start Message
     printf("Ninja Virtual Machine started\n");
 
-    /* Stop Message */
+    // Stop Message
     printf("Ninja Virtual Machine stopped\n");
 }
