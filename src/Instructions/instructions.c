1e2df98f387d9d8f5c9c94fa1c8f3782c71a85ed
Max Stephan <max.stephan@mni.thm.de>
2019-10-16 11:06:08 +0200

    added instructions

---
+++ src/Instructions/instructions.c
@@ -0,0 +1,7 @@
+#include "instructions.h"
+
+void add() {}
+void sub() {}
+void mul() {}
+void div() {}
+void mod() {}
---
+++ src/Instructions/instructions.h
@@ -0,0 +1,10 @@
+#ifndef NJCM_Instructons_H_INCLUDED
+#define NJCM_Instructons_H_INCLUDED
+
+void add();
+void sub();
+void mul();
+void div();
+void mod();
+
+#endif
