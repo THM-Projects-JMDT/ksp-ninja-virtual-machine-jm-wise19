14e008cb3e746225e5c05ffbf16f30e9f29f0df1
Max Stephan <max.stephan@mni.thm.de>
2019-10-16 19:01:09 +0200

    fixed formatting

--- src/njvm.c
+++ src/njvm.c
@@ -5,55 +5,47 @@
 #define VERSION 0
 
 // Handle version argument
-static void version(const char *myself)
-{
-    printf("Ninja Virtual Machine Version %d (compiled %s, %s)\n", VERSION,
-           __DATE__, __TIME__);
+static void version(const char *myself) {
+  printf("Ninja Virtual Machine Version %d (compiled %s, %s)\n", VERSION,
+         __DATE__, __TIME__);
 }
 
 // Handle help argument
-static void help(const char *myself)
-{
-    printf("usage: %s [options] ...\n"
-           "Options: \n"
-           "\t--version \t show version and exit\n"
-           "\t--help \t\t show this text and exit\n",
-           myself);
+static void help(const char *myself) {
+  printf("usage: %s [options] ...\n"
+         "Options: \n"
+         "\t--version \t show version and exit\n"
+         "\t--help \t\t show this text and exit\n",
+         myself);
 }
 
 // Handle an invalid argument
-static void invalid(const char *myself, const char *arg)
-{
-    printf("unknown command line argument '%s', try '%s --help'\n", arg, myself);
+static void invalid(const char *myself, const char *arg) {
+  printf("unknown command line argument '%s', try '%s --help'\n", arg, myself);
 }
 
-int main(int argc, char *argv[])
-{
-    for (int i = 1; i < argc; i++)
-    {
-        // argument version
-        if (strcmp(argv[i], "--version") == 0)
-        {
-            version(argv[0]);
-            exit(0);
-        }
-        // argument help
-        else if (strcmp(argv[i], "--help") == 0)
-        {
-            help(argv[0]);
-            exit(0);
-        }
-        // handle an invalid argument
-        else
-        {
-            invalid(argv[0], argv[i]);
-            exit(0);
-        }
+int main(int argc, char *argv[]) {
+  for (int i = 1; i < argc; i++) {
+    // argument version
+    if (strcmp(argv[i], "--version") == 0) {
+      version(argv[0]);
+      exit(0);
     }
+    // argument help
+    else if (strcmp(argv[i], "--help") == 0) {
+      help(argv[0]);
+      exit(0);
+    }
+    // handle an invalid argument
+    else {
+      invalid(argv[0], argv[i]);
+      exit(0);
+    }
+  }
 
-    // Start Message
-    printf("Ninja Virtual Machine started\n");
+  // Start Message
+  printf("Ninja Virtual Machine started\n");
 
-    // Stop Message
-    printf("Ninja Virtual Machine stopped\n");
+  // Stop Message
+  printf("Ninja Virtual Machine stopped\n");
 }
