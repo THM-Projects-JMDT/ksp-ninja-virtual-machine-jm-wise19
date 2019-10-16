605c4c7c8e7174bec6164b4cfb29bbc10e70aed3
Jannik Lapp <jannik.lapp@mni.thm.de>
2019-10-16 20:43:45 +0200

    added programs and define values

--- src/njvm.c
+++ src/njvm.c
@@ -3,12 +3,35 @@
 #include <string.h>
 
 #define VERSION 0
+#define HALT 0
+#define PUSHC 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define DIV 5
+#define MOD 6
+#define RDINT 7
+#define WRINT 8
+#define RDCHR 9
+#define WRCHR 10
+#define IMMEDIATE(x) ((x)&0x00FFFFFF)
+unsigned int code1[][3] = {
+    {(PUSHC << 24) | IMMEDIATE(3), (PUSHC << 24) | IMMEDIATE(4), (ADD << 24),
+     (PUSHC << 24) | IMMEDIATE(10), (PUSHC << 24) | IMMEDIATE(6), (SUB << 24),
+     (MUL << 24), (WRINT << 24), (PUSHC << 24) | IMMEDIATE(10), (WRCHR << 24),
+     (HALT << 24)},
+    {(PUSHC << 24) | IMMEDIATE(-2), (RDINT << 24), (MUL << 24),
+     (PUSHC << 24) | IMMEDIATE(3), (ADD << 24), (WRINT << 24),
+     (PUSHC << 24) | IMMEDIATE('\n'), (WRCHR << 24), (HALT << 24)},
+    {(RDCHR << 24), (WRINT << 24), (PUSHC << 24) | IMMEDIATE('\n'),
+     (WRCHR << 24), (HALT << 24)}};
 
 // Handle version argument
 static void version(const char *myself) {
   printf("Ninja Virtual Machine Version %d (compiled %s, %s)\n", VERSION,
          __DATE__, __TIME__);
 }
+static void prog(const int i) {}
 
 // Handle help argument
 static void help(const char *myself) {
@@ -34,6 +57,15 @@ int main(int argc, char *argv[]) {
     // argument help
     else if (strcmp(argv[i], "--help") == 0) {
       help(argv[0]);
+      exit(0);
+    } else if (strcmp(argv[i], "--prog1") == 0) {
+      prog(1);
+      exit(0);
+    } else if (strcmp(argv[i], "--prog2") == 0) {
+      prog(2);
+      exit(0);
+    } else if (strcmp(argv[i], "--prog3") == 0) {
+      prog(3);
       exit(0);
     }
     // handle an invalid argument
