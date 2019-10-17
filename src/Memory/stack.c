d3ab63f8dfe3c5d31f8a3f6251384d179542d4dd
Max Stephan <max.stephan@mni.thm.de>
2019-10-17 12:12:21 +0200

    moved stack

---
+++ src/Memory/stack.c
@@ -0,0 +1,21 @@
+#include "stack.h"
+#include "../util/error.h"
+
+#define SK_SIZE 20
+
+static int sp = 0;
+static int stack[SK_SIZE];
+
+void push(int value) {
+  if (sp == SK_SIZE)
+    stackOverflowError();
+  stack[sp] = value;
+  sp++;
+}
+
+int pop() {
+  if (sp == 0)
+    stackUnderflowError();
+  sp--;
+  return stack[sp];
+}
---
+++ src/Memory/stack.h
@@ -0,0 +1,7 @@
+#ifndef NJCM_STACK_H_INCLUDED
+#define NJCM_STACK_H_INCLUDED
+
+void push(int value);
+int pop();
+
+#endif
