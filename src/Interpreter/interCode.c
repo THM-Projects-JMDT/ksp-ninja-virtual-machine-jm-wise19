059511ca422e6e5c4267fb69127754f5b93a0894
Max Stephan <max.stephan@mni.thm.de>
2019-10-17 12:13:04 +0200

    added Code interpreter

---
+++ src/Interpreter/interCode.c
@@ -0,0 +1,107 @@
+#include "interCode.h"
+#include "../Memory/programMemory.h"
+#include "../Memory/stack.h"
+#include "../util/error.h"
+#include <stdio.h>
+
+#define GET_INST(i) (((i)&0xFF000000) >> 24)
+#define SIGN_EXTEND(i) ((i)&0x00800000 ? (i) | 0xFF000000 : (i))
+#define GET_IMMEDIATE(i) ((i)&0x00FFFFFF)
+
+static int pc = 0;
+static int haltProg = 0;
+// Instruction definition (starts with 0)
+typedef enum inst {
+  halt,
+  pushc,
+  add,
+  sub,
+  mul,
+  div,
+  mod,
+  rdint,
+  wrint,
+  rdchr,
+  wrchr
+} inst;
+
+// Instructions Functions
+static void execHalt() { haltProg = 1; }
+static void execPushc(int value) { push(value); }
+static void execAdd() { push(pop() + pop()); }
+static void execSub() { push(pop() - pop()); }
+static void execMul() { push(pop() * pop()); }
+static void execDiv() {
+  // Check if Secound nummber is Zero
+  int num1 = pop();
+  int num2 = pop();
+
+  if (num2 == 0)
+    dividedByZeroError();
+
+  push(num1 / num2);
+}
+static void execMod() { push(pop() % pop()); }
+static void execRdint() { /*TODO*/
+}
+static void execWrint() { /*TODO*/
+}
+static void execRdchr() { /*TODO*/
+}
+static void execWrchr() { /*TODO*/
+}
+
+// Find right Instruction
+void execInst(const unsigned int inst) {
+  int opcode = GET_INST(inst);
+  int value = SIGN_EXTEND(GET_IMMEDIATE(inst));
+
+  switch (opcode) {
+  case halt:
+    execHalt();
+    break;
+  case pushc:
+    execPushc(value);
+    break;
+  case add:
+    execAdd();
+    break;
+  case sub:
+    execSub();
+    break;
+  case mul:
+    execMul();
+    break;
+  case div:
+    execDiv();
+    break;
+  case mod:
+    execMod();
+    break;
+  case rdint:
+    execRdint();
+    break;
+  case wrint:
+    execWrint();
+    break;
+  case rdchr:
+    execRdchr();
+    break;
+  case wrchr:
+    execWrchr();
+    break;
+
+  default:
+    unknownInstructionError(opcode);
+    break;
+  }
+}
+
+void execprog() {
+  while (!haltProg) {
+    int ir = programMemory[pc];
+    pc++;
+    execInst(ir);
+  }
+  pc = 0;
+}
---
+++ src/Interpreter/interCode.h
@@ -0,0 +1,7 @@
+#ifndef NJCM_Instructons_H_INCLUDED
+#define NJCM_Instructons_H_INCLUDED
+
+void execInst(const unsigned inst);
+void execprog();
+
+#endif
