5fd0c71731fe70ee0dc3aad817a2883c50fd7138
Jannik Lapp <jannik.lapp@mni.thm.de>
2019-10-22 15:29:30 +0200

    changed main load method

--- src/njvm.c
+++ src/njvm.c
@@ -7,59 +7,63 @@
 #define VERSION 1
 
 // Handle version argument
-static void version(const char *myself) {
+static void version(const char *myself)
+{
   printf("Ninja Virtual Machine Version %d (compiled %s, %s)\n", VERSION,
          __DATE__, __TIME__);
 }
 
 // Handle help argument
-static void help(const char *myself) {
+static void help(const char *myself)
+{
   printf("usage: %s [options] ...\n"
          "Options: \n"
-         "\t--prog1 \t run program 1\n"
-         "\t--prog2 \t run program 2\n"
-         "\t--prog3 \t run program 3\n"
          "\t--version \t show version and exit\n"
          "\t--help \t\t show this text and exit\n",
          myself);
 }
 
 // Handle an invalid argument
-static void invalid(const char *myself, const char *arg) {
+static void invalid(const char *myself, const char *arg)
+{
   printf("unknown command line argument '%s', try '%s --help'\n", arg, myself);
 }
 
-int main(int argc, char *argv[]) {
-  for (int i = 1; i < argc; i++) {
+int main(int argc, char *argv[])
+{
+  for (int i = 1; i < argc; i++)
+  {
     // argument version
-    if (strcmp(argv[i], "--version") == 0) {
+    if (strcmp(argv[i], "--version") == 0)
+    {
       version(argv[0]);
       exit(0);
     }
     // argument help
-    else if (strcmp(argv[i], "--help") == 0) {
+    else if (strcmp(argv[i], "--help") == 0)
+    {
       help(argv[0]);
       exit(0);
       // argument prog 1-3
-    } else if (strcmp(argv[i], "--prog1") == 0) {
-      loadprog(0);
-    } else if (strcmp(argv[i], "--prog2") == 0) {
-      loadprog(1);
-    } else if (strcmp(argv[i], "--prog3") == 0) {
-      loadprog(2);
     }
     // handle an invalid argument
-    else {
-      invalid(argv[0], argv[i]);
-      exit(0);
+    else
+    {
+      if ((argv[i][0] == '-' && argv[i][1] == '-'))
+      {
+        invalid(argv[0], argv[argc - 1]);
+        exit(0);
+      }
+      printf("Programm loaded\n");
+      //loadprog(argv[i]);
     }
   }
 
   // Start Message
   printf("Ninja Virtual Machine started\n");
 
-  execList();
-  execprog();
+  // execList();
+  // execprog();
 
   // Stop Message
   printf("Ninja Virtual Machine stopped\n");
