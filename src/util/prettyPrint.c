2d2146af63f5d529d86ec1a907fa04ef43e8a007
Max Stephan <max.stephan@mni.thm.de>
2019-10-30 19:58:48 +0100

    added prettyPrint

---
+++ src/util/prettyPrint.c
@@ -0,0 +1,23 @@
+#include "prettyPrint.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void setFormat(FILE *stream, const formats format) {
+  fprintf(stream, "\033[%dm", format);
+}
+
+void pprintf(const formats format, const char *msg, ...) {
+  va_list parms;
+
+  // Change Color
+  setFormat(stdout, format);
+
+  // print String with parms
+  va_start(parms, msg);
+  vprintf(msg, parms);
+  va_end(parms);
+
+  // Reset Color
+  setFormat(stdout, RESET_FORMAT);
+}
---
+++ src/util/prettyPrint.h
@@ -0,0 +1,33 @@
+#ifndef NJVM_PRETTYPRITN_H_INCLUDED
+#define NJVM_PRETTYPRITN_H_INCLUDED
+
+#include <stdio.h>
+
+typedef enum
+{
+  RESET_FORMAT,
+  BOLD,
+  UNDERLINE = 4,
+  INVERSE = 7,
+  BLACK = 30,
+  RED,
+  GREEN,
+  YELLOW,
+  BLUE,
+  MAGENTA,
+  CYAN,
+  WHITE,
+  BG_BLACK = 40,
+  BG_RED,
+  BG_GREEN,
+  BG_YELLOW,
+  BG_BLUE,
+  BG_MAGENTA,
+  BG_CYAN,
+  BG_WHITE,
+} formats;
+
+void pprintf(const formats color, const char *msg, ...);
+void setFormat(FILE *stream, const formats format);
+
+#endif
