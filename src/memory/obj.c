7aa3c052375dab7861216973e4ee6290beaf0596
Max Stephan <max.stephan@mni.thm.de>
2019-11-19 15:20:29 +0100

    added createObjRef

--- src/memory/obj.c
+++ src/memory/obj.c
@@ -1,1 +1,31 @@
 #include "obj.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+ObjRef createObjRef(unsigned int size) {
+  ObjRef objRef = malloc(sizeof(unsigned int) + size);
+  objRef->size = size;
+
+  return objRef;
+}
+
+ObjRef createObjRefInt(int value) {
+  ObjRef objRef = createObjRef(sizeof(int));
+  *(int *)objRef->data = value;
+
+  return objRef;
+}
+
+ObjRef createObjRefChar(char value) {
+  ObjRef objRef = createObjRef(sizeof(char));
+  *(char *)objRef->data = value;
+
+  return objRef;
+}
+
+ObjRef createObjRefBool(bool value) {
+  ObjRef objRef = createObjRef(sizeof(bool));
+  *(bool *)objRef->data = value;
+
+  return objRef;
+}
--- src/memory/obj.h
+++ src/memory/obj.h
@@ -1,9 +1,15 @@
 #ifndef NJCM_OBJ_H_INCLUDED
 #define NJCM_OBJ_H_INCLUDED
 
+#include <stdbool.h>
+
 typedef struct {
   unsigned int size;     /* byte count of payload data */
   unsigned char data[1]; /* payload data , size as needed */
 } * ObjRef;
+
+ObjRef createObjRefInt(int value);
+ObjRef createObjRefChar(char value);
+ObjRef createObjRefBool(bool value);
 
 #endif
