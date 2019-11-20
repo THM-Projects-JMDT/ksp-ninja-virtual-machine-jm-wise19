96b99ef4cb700aaf5d98fb3e3a6dbd4f97bf996f
Max Stephan <max.stephan@mni.thm.de>
2019-11-20 07:51:44 +0100

    added objRef Null check

--- src/memory/obj.c
+++ src/memory/obj.c
@@ -1,9 +1,15 @@
 #include "obj.h"
+#include "../util/error.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 ObjRef newIntObj(int value) {
   ObjRef objRef = malloc(sizeof(unsigned int) + sizeof(int));
+
+  // check if malloc worked
+  if (objRef == NULL)
+    outOfMemoryError();
+
   objRef->size = sizeof(int);
   *(int *)objRef->data = value;
 
