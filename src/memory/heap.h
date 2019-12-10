e418fd96a06222560b918271c7356c12e84c1ae9
Max Stephan <max.stephan@mni.thm.de>
2019-12-10 14:42:38 +0100

    added brockenheart

--- src/memory/heap.h
+++ src/memory/heap.h
@@ -5,6 +5,11 @@
 #ifndef NJCM_HEAP_H_INCLUDED
 #define NJCM_HEAP_H_INCLUDED
 
+#define BROKEN_HEART (1 << (sizeof(unsigned int) * 8 - 2))
+#define SET_BROKEN_HEART(obj) ((obj)->size = (obj)->size | BROKEN_HEART)
+#define GET_BROKEN_HEART(obj)                                                  \
+  (((obj)->size & BROKEN_HEART) >> (sizeof(unsigned int) * 8 - 2))
+
 void initHeap(int size);
 void switchHeap(void);
 void *allocOnHeap(const int size);
